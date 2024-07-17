dc6ed128a210f83bc68746918e2276f19f4edc0c
Agoro Gafar <agorogafar92@gmail.com>
2024-07-17 09:05:00 +0100

    news

--- news.php
+++ news.php
@@ -1,6 +1,79 @@
 <?php
 include_once('asset/header.php');
 ?>
-<main>
-   <h1>News</h1> 
-</main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   
+
+<!-- Campus, Events and Notice Board HTML Starts -->
+<div class="main-section" style="background:#f0f3fa">
+  <div class="container">
+    <!-- Campus News Section -->
+    <div class="event">
+      <h2 class="heading">Campus News</h2>
+      <div>
+        <marquee direction="up" scrollamount="7" style="height:340px;">
+          <ul>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+            <li>
+              <i>01-April-2023 :</i> Lorem Ipsum is simply dummy text of the printing and typesetting industry. <img src="images/new.gif">
+            </li>
+          </ul>
+        </marquee>
+      </div>
+    </div>
+    <!-- Events Section -->
+    <div class="event">
+      <h2 class="heading">Events</h2>
+      <div>
+        <ul>
+          <li>
+            <span class="event-date">29 <br> April </span> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>
+          <li>
+            <span class="event-date">29 <br> April </span> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>
+          <li>
+            <span class="event-date">29 <br> April </span> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>
+          <li>
+            <span class="event-date">29 <br> April </span> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>
+          <li>
+            <span class="event-date">29 <br> April </span> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- Notice Board Section -->
+    <div class="event">
+      <h2 class="heading">Notice Boards</h2>
+      <div>
+        <ul>
+          <li>
+            <img src="logo.png" alt="">
+      </body>
+</html>
